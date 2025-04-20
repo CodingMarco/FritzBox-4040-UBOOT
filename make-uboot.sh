3de19548cdb02d3892b558b951a1a5665978a523
Marco von Rosenberg <codingmarco@gmail.com>
2025-04-21 00:33:01 +0200

    Make with nproc parallell processes

--- make-uboot.sh
+++ make-uboot.sh
@@ -33,13 +33,13 @@ arm-openwrt-linux-ld -v || no_toolchain
 make clean || die "Can't clean old cruft"
 
 if [[ $BOARDNAME == "huawei_ap4050dn" ]]; then
-	USE_PRIVATE_LIBGCC=yes make -j30 "$BOARDNAME"_ramboot || die "Failed to set build target"
-	USE_PRIVATE_LIBGCC=yes make -j30 || die "Failure during u-boot build"
+	USE_PRIVATE_LIBGCC=yes make -j$(nproc) "$BOARDNAME"_ramboot || die "Failed to set build target"
+	USE_PRIVATE_LIBGCC=yes make -j$(nproc) || die "Failure during u-boot build"
 	mv u-boot.bin u-boot_ramboot.bin
 fi
 
-USE_PRIVATE_LIBGCC=yes make -j30 $BOARDNAME || die "Failed to set build target"
-USE_PRIVATE_LIBGCC=yes make -j30 || die "Failure during u-boot build"
+USE_PRIVATE_LIBGCC=yes make -j$(nproc) $BOARDNAME || die "Failed to set build target"
+USE_PRIVATE_LIBGCC=yes make -j$(nproc) || die "Failure during u-boot build"
 
 [ -e u-boot.bin ] || die "Build succeeded. But u-boot.bin wasn't created"
 
