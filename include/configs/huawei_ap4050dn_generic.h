8553d1c69caebebd5d8f6bb5bfa87bdeaf09798f
Marco von Rosenberg <codingmarco@gmail.com>
2025-04-20 18:17:44 +0200

    Set baudrate to 9600 baud instead of 115200 baud if we're a Huawei AP4050DN

--- include/configs/huawei_ap4050dn_generic.h
+++ include/configs/huawei_ap4050dn_generic.h
@@ -7,23 +7,21 @@
 #define CONFIG_MODEL_HUAWEI_AP4050DN
 #define CONFIG_MODEL		"AP4050DN"
 #define MTDIDS_DEFAULT		"nand0=nand0" // TODO
-#define MTDPARTS_DEFAULT	"mtdparts=nand0:256k(0:SBL1),256k(0:MIBIB),512k(0:QSEE),256k(0:CDT),256k(0:DDRPARAMS),1024k(0:APPSBL),256k(0:ART),256k(0:APPSBLENV),512k(ResultA),2560k(configA),1024k(bootimageA),25600k(SysImageA),256k(1:SBL1),256k(1:MIBIB),512k(1:QSEE),256k(1:CDT),256k(1:DDRPARAMS),1024k(1:APPSBL),512k(Reservel),512k(ResultB),2560k(configB),1024k(bootimageB),25600k(SysImageB)"
+#define MTDPARTS_DEFAULT    "mtdparts=nand0:256k(0:SBL1),256k(0:MIBIB),512k(0:QSEE),256k(0:CDT),256k(0:DDRPARAMS),1024k(0:APPSBL),256k(0:ART),256k(0:APPSBLENV),512k(ResultA),2560k(configA),1024k(bootimageA),512k(uboot),25088k(SysImageA),256k(1:SBL1),256k(1:MIBIB),512k(1:QSEE),256k(1:CDT),256k(1:DDRPARAMS),1024k(1:APPSBL),512k(Reservel),512k(ResultB),2560k(configB),1024k(bootimageB),25600k(SysImageB)"
 
 #undef CONFIG_BOOTDELAY
 #define CONFIG_BOOTDELAY 3
 
 #undef CONFIG_BOOTCOMMAND
-#define CONFIG_BOOTCOMMAND		"run huaweiboot" // TODO
+#define CONFIG_BOOTCOMMAND      "run huaweiboot" // TODO
 
-#define CONFIG_MAC_PARTITION "ResultA"  // TODO: ???
+#define CONFIG_MAC_PARTITION    "ResultA"  // TODO: ???
 
 #define CONFIG_EXTRA_ENV_SETTINGS				\
     "mtdids=" MTDIDS_DEFAULT "\0"				\
     "mtdparts=" MTDPARTS_DEFAULT "\0"			\
-    "tftpboot=tftpboot && bootm; sleep 5; run tftpboot\0"	\
-    "nandboot=nboot SysImageA && bootm\0"			\
-    "huaweiboot=run nandboot;\0"		\
-    "bootargs=console=ttyMSM0,115200n8 root=/dev/mtdblock24 rootfstype=squashfs\0"		\
+    "huaweiboot=nboot SysImageA && bootm\0"		\
+    "bootargs=console=ttyMSM0,9600n8\0"        \
 
 #undef V_PROMPT
 #define V_PROMPT		"(" CONFIG_MODEL ") # "
@@ -60,8 +58,6 @@
 #define CONFIG_CMD_SAVES	/* save S record dump		*/
 #define CONFIG_CMD_SDRAM	/* SDRAM DIMM SPD info printout */
 #define CONFIG_CMD_SETEXPR	/* setexpr support		*/
-
-
 
 #undef CONFIG_SYS_LOAD_ADDR
 #define CONFIG_SYS_LOAD_ADDR    0x85000000
